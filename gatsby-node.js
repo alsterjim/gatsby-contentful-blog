b0d709fea954f5f808fd702c8af59cbede136d31
Jim Löfgren <jim.lofgren@isotop.se>
2021-04-09 13:28:40 +0200

    adding some typescript

--- gatsby-node.js
+++ gatsby-node.js
@@ -30,7 +30,7 @@ exports.createPages = async ({ graphql, actions }) => {
   result.data.allContentfulBlogPost.edges.forEach(({ node }) => {
     createPage({
       path: node.slug,
-      component: path.resolve(`./src/templates/blog-post.js`),
+      component: path.resolve(`./src/templates/blog-post.tsx`),
       context: {
         // Data passed to context is available
         // in page queries as GraphQL variables.
--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -3,6 +3,8 @@ import { graphql } from "gatsby"
 
 export default function BlogPost({ data }) {
   const post = data.contentfulBlogPost
+  var i = 1
+  i = "test"
   return (
       <div>
         <h1>{post.title}</h1>
