ea1c0fff6b918c819ae09132925fb48b88d0470e
Jim Löfgren <jim.lofgren@isotop.se>
2021-04-13 15:10:11 +0200

    moar bootstrap

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -8,7 +8,7 @@ interface IBlogPost {
 export default function BlogPost({ data }: IBlogPost) {
   const post = data.contentfulBlogPost;
   return (
-    <div>
+    <div class="container">
       <h1>{post.title}</h1>
       <div
         dangerouslySetInnerHTML={{ __html: post.body.childMarkdownRemark.html }}
